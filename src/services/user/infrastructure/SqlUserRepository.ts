5d23ed79878c089cb3d9a073a52d61118b80ac43
derrmru <pjsweeney1@live.co.uk>
2025-09-01 17:40:27 +0200

    reorg

--- src/services/user/infrastructure/SqlUserRepository.ts
+++ src/services/user/infrastructure/SqlUserRepository.ts
@@ -1,20 +1,28 @@
-import { PrismaClient } from "@prisma/client";
+import { Prisma, PrismaClient } from "@prisma/client";
 import { User } from "../domain/User";
 
 export class SqlUserRepository {
   constructor(private db: PrismaClient) {}
 
   async findUserByExternalId(externalId: string) {
-    const result = await this.db.user.findUnique({
-      where: { externalId },
-    });
+    const result = await this.db.$transaction(
+      async (tx: Prisma.TransactionClient) => {
+        return await tx.user.findUnique({
+          where: { externalId },
+        });
+      }
+    );
     return User.create({ ...result });
   }
 
   async findById(id: number) {
-    const result = await this.db.user.findUnique({
-      where: { id },
-    });
+    const result = await this.db.$transaction(
+      async (tx: Prisma.TransactionClient) => {
+        return await tx.user.findUnique({
+          where: { id },
+        });
+      }
+    );
     return User.create({ ...result });
   }
 }
